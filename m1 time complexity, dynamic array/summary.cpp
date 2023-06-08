3f498b52738cec522a0dbae6b32cda56ffa3a4f0
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2023-06-08 06:50:42 +0600

    big O, time & space complexity

---
+++ m1 time complexity, dynamic array/summary.cpp
@@ -0,0 +1,42 @@
+/*
+
+Big O Notation -> (steps)
+1. grammer
+2. input
+
+GRAMMER
+1. always calculate for worst case
+2. ignore the constants
+
+## NORMALLY, complexity comes from loop
+
+## if program have no loop then it's complexity is O(1)
+
+## how time is calculated from time complexity
+10^7 = 1s
+10^8 = 10s
+10^9 = 100s
+10^10 = 1000s
+
+3 * 10^7 is max
+O(N) -> 10^7 max
+
+
+----------------------------------------------------------
+O(1)
+O(logN) -> 10^18
+O(sqrt(N)) -> 10^14
+O(N) -> 10^7
+O(NlogN) -> 10^5 and for 2s 10^6
+O(N*N) -> 10^3
+
+
+----------------------------------------------------------
+## SPACE complexity
+
+int sum = a+b;
+1. input space // a+b
+2. auxiliary space // sum
+
+
+*/
